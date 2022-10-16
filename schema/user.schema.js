6e3c4b50278b4a6965e254c76ec06bf2c9cf618b
aldaine-AG <aldaineclarke1@gmail.com>
2022-10-16 08:50:45 -0500

    create methods on schema for password reset request and duplicate check

--- schema/user.schema.js
+++ schema/user.schema.js
@@ -1,10 +1,12 @@
 const {model, Schema} = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
+const emailer = require("../utilities/nodemailer.utility");
+const htmlCompiler = require("../utilities/compileHtml.utility");
 
 const userSchema = new Schema({
     first_name : {
         type: String, 
-        unique: [true, "name already exist in the database"]
+        required:[true,"First name is a required field"]
     }, 
     last_name : {
         type: String, 
@@ -15,8 +17,10 @@ const userSchema = new Schema({
     },
     email: {
         type: String, 
-        unique: [true, "Email already exist in the database"]
+        unique: true,
+        required:[true, "Email is a required field"]
     },
+
     password: {
         type: String, 
         required: [true, "Password was not provided"]
@@ -59,6 +63,16 @@ userSchema.pre("findOneAndUpdate", async function(next){
     }
 });
 
+userSchema.methods.checkDupe = function () {
+	return new Promise(async (resolve, reject) => {
+		const dupe = await model('User')
+			.find({ email: this.email})
+			.catch((err) => {
+				reject(err)
+			})
+		resolve(dupe.length > 0)
+	})
+}
 
 userSchema.post(/^find/, async function(doc){
     if(Array.isArray(doc)){
@@ -86,6 +100,20 @@ function removeSensitiveFields(doc){
 userSchema.methods.isCorrectPassword = async function(password){
     let isCorrect = await bcrypt.compare(password, this.password);
     return isCorrect;
+}
+
+
+userSchema.methods.requestPasswordReset = async function(user, redirectLink){
+    const data = {
+        user: user,
+        redirectLink
+    }
+    try{
+        let html = htmlCompiler("password_reset",data);
+        await emailer.sendMail(user.email, "Password Reset","",html);
+    }catch(error){
+        Promise.reject(new Error(error));
+    }
 }
 
 
