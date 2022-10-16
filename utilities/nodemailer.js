0a6a01837661467a8060004b3e18e8e6ed7a2924
aldaine-AG <aldaineclarke1@gmail.com>
2022-10-16 02:43:20 -0500

    added utilites to the project

---
+++ utilities/nodemailer.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+const { SMTP_HOST, SMTP_PORT, UMAIL, UPASS } = process.env
+var nodemailer = require('nodemailer')
+
+class Emailer {
+	#transporter = nodemailer.createTransport({
+        host:SMTP_HOST,
+        port:SMTP_PORT,
+        secure:true,
+		auth: {
+			user: UMAIL,
+			pass: UPASS,
+		},
+	})
+
+	constructor() {}
+	/**
+	 * Sends an email to the intended recipient.
+	 * @param {string} to - The recipient or recipient array for the email
+	 * @param {string} sub - The subject of the email
+	 * @param {string} body - The body of the email
+	 */
+	sendMail(to, sub, body, attachment) {
+		let selectedAttachment = [];
+		// check if attachment is an array;
+		if(attachment){
+			if(Array.isArray(attachment)){
+				selectedAttachment = attachment;
+			}else{
+				selectedAttachment.push(attachment);
+			}
+		}
+		
+		let mailOptions = {
+			to: to,
+			from: UMAIL,
+			subject: sub,
+			text: body,
+			attachments:selectedAttachment,
+		}
+		this.#transporter.sendMail(mailOptions, function (error, info) {
+			if (error) {
+				console.error(error)
+				throw error
+			} else {
+				console.log('Email sent: ' + info.response)
+			}
+		})
+	}
+}
+
+module.exports = new Emailer()
